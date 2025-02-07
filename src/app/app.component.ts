848a8ea93321327f7b5ff0acf9a39cf368f76f88
gruchh <kubol2@gmail.com>
2025-02-07 21:44:18 +0100

    Todo - static list with table implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,50 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { TodoItem } from './TodoItem';
+import { TodoList } from './TodoList';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatBadgeModule,
+    MatTableModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'Angular-ToDoList';
+  private list = new TodoList(`Jake`, [
+    new TodoItem('Go for run', true),
+    new TodoItem('Get flowers'),
+    new TodoItem('Collect tickets'),
+  ]);
+  get username(): string {
+    return this.list.user;
+  }
+  get itemCount(): number {
+    return this.list.items.filter((item) => !item.complete).length;
+  }
+  get items(): readonly TodoItem[] {
+    return this.list.items.filter((item) => !item.complete);
+  }
+  addItem(newItem: string) {
+    if (newItem != '') {
+      this.list.addItem(newItem);
+    }
+  }
 }
